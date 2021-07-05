4c071c73fe0181ac7f84c11a3cf8618d0175d57e
Alexandre <alexandre.parra@premiersoft.net>
2021-07-05 11:00:21 -0300

    Added extensions file

--- app/src/main/java/com/alexparra/bankaccountapp/utils/StringExtension.kt
+++ app/src/main/java/com/alexparra/bankaccountapp/utils/StringExtension.kt
@@ -1,2 +1,8 @@
 package com.alexparra.bankaccountapp.utils
 
+import java.security.MessageDigest
+
+fun String.toSHA256(): String {
+    val messageDigest = MessageDigest.getInstance("SHA-256").digest(toByteArray())
+    return messageDigest.fold("", { str, it -> str + "%02x".format(it) })
+}
