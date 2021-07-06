0405d4e101b211bb13ce4601a3f83866bd7e40e3
Alexandre <alexandre.parra@premiersoft.net>
2021-07-06 15:14:03 -0300

    Added extensions for ease of use

--- app/src/main/java/com/alexparra/bankaccountapp/utils/ViewExtensions.kt
+++ app/src/main/java/com/alexparra/bankaccountapp/utils/ViewExtensions.kt
@@ -1,22 +1,42 @@
 package com.alexparra.bankaccountapp.utils
 
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
-import com.alexparra.bankaccountapp.R
 
-fun Fragment.replaceFragment() {
-    activity?.supportFragmentManager?.beginTransaction()?.let {
-        it.setReorderingAllowed(true)
-        it.replace(R.id.fragment_container_view, this)
+fun FragmentActivity.replaceFragment(
+    fragment: Fragment,
+    containerId: Int,
+    tag: String? = null,
+    addToStack: Boolean = true,
+    stackName: String? = null
+) {
+    supportFragmentManager.commit {
+        replace(containerId, fragment, tag)
+        if (addToStack) {
+            supportFragmentManager.findFragmentById(containerId)?.let {
+                addToBackStack(stackName)
+            }
+        }
     }
 }
 
-fun Fragment.addFragmentWithArgs(key: String, value: Any) {
-    val bundle = bundleOf(key to value)
-    activity?.supportFragmentManager?.commit {
-        setReorderingAllowed(true)
-        add<AccountScreenFragment>(R.id.fragment_container_view, args = bundle)
-    }
+fun Fragment.replaceFragment(
+    fragment: Fragment,
+    containerId: Int,
+    tag: String? = null,
+    addToStack: Boolean = true,
+    stackName: String? = null
+) {
+    activity?.replaceFragment(
+        fragment,
+        containerId,
+        tag,
+        addToStack,
+        stackName
+    )
+}
+
+fun Fragment.popBackStack(stackName: String? = null, flags: Int = 0) {
+    activity?.supportFragmentManager?.popBackStackImmediate(stackName, flags)
 }
