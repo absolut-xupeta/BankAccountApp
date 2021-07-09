58670ea86124f43a1c6bf3eda93189931b79f876
Alexandre <alexandre.parra@premiersoft.net>
2021-07-09 16:56:30 -0300

    Started new recyclerview for accountscreen

--- app/src/main/java/com/alexparra/bankaccountapp/TransactionFragment.kt
+++ app/src/main/java/com/alexparra/bankaccountapp/TransactionFragment.kt
@@ -5,13 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.alexparra.bankaccountapp.databinding.FragmentLoginBinding
 import com.alexparra.bankaccountapp.databinding.FragmentTransactionBinding
 import com.alexparra.bankaccountapp.model.CustomAdapter
 import com.alexparra.bankaccountapp.objects.AccountsManager
