3c1445731c1f14b973259a93a605960ed9ee831e
Alexandre <alexandre.parra@premiersoft.net>
2021-07-09 14:21:17 -0300

    Transaction screen and recycler view logic

--- app/src/main/java/com/alexparra/bankaccountapp/TransactionFragment.kt
+++ app/src/main/java/com/alexparra/bankaccountapp/TransactionFragment.kt
@@ -5,14 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alexparra.bankaccountapp.databinding.FragmentLoginBinding
+import com.alexparra.bankaccountapp.databinding.FragmentTransactionBinding
+import com.alexparra.bankaccountapp.model.CustomAdapter
+import com.alexparra.bankaccountapp.objects.AccountsManager
 
 class TransactionFragment : Fragment() {
 
+    private val args: TransactionFragmentArgs by navArgs()
+
+    private lateinit var binding: FragmentTransactionBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false)
+    ): View {
+        binding = FragmentTransactionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        recyclerView()
+    }
+
+    /**
+     * Display recycler view if transaction history is not null.
+     */
+    private fun recyclerView() {
+        val transactions = AccountsManager.loadAccountTransaction(args.id)
+
+        if (transactions != null) {
+            val recyclerView: RecyclerView = binding.recycler
+            val customAdapter = CustomAdapter(transactions)
+
+            recyclerView.apply {
+                addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                adapter = customAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+
+        } else {
+            displayNothing()
+        }
+    }
+
+    /**
+     * If the recyclerView is empty display only a warning message.
+     */
+    private fun displayNothing() {
+        binding.recycler.apply {
+            visibility = View.GONE
+        }
+
+        binding.noTransaction.apply {
+            visibility = View.VISIBLE
+        }
     }
 }
