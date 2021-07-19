69cf5c8d8121e3d83ff556e753e8553b3ec17c1a
Alexandre <alexandre.parra@premiersoft.net>
2021-07-19 13:26:59 -0300

    Refactored code and simplified fragment

--- app/src/main/java/com/alexparra/bankaccountapp/fragments/TicTacToeFragment.kt
+++ app/src/main/java/com/alexparra/bankaccountapp/fragments/TicTacToeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
@@ -13,10 +12,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.alexparra.bankaccountapp.R
 import com.alexparra.bankaccountapp.adapters.TicTacToeAdapter
 import com.alexparra.bankaccountapp.databinding.FragmentTicTacToeBinding
+import com.alexparra.bankaccountapp.objects.TicTacToeManager
 import com.alexparra.bankaccountapp.objects.TicTacToeManager.board
+import com.alexparra.bankaccountapp.objects.TicTacToeManager.checkStatus
 import com.alexparra.bankaccountapp.objects.TicTacToeManager.isBotRound
 import com.alexparra.bankaccountapp.objects.TicTacToeManager.roundCount
 import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
 
 class TicTacToeFragment : Fragment() {
 
@@ -40,18 +42,17 @@ class TicTacToeFragment : Fragment() {
     }
 
     private fun ticTacToe() {
-        ticTacToeAdapter.reset()
-        resetViews()
-
         val recyclerView: RecyclerView = binding.recyclerView
 
         ticTacToeAdapter = TicTacToeAdapter(board, ::onInteraction)
+
+        ticTacToeAdapter.reset()
+        resetViews()
 
         recyclerView.apply {
             adapter = ticTacToeAdapter
             layoutManager = GridLayoutManager(context, 3)
         }
-
     }
 
     private fun onInteraction(state: TicTacToeAdapter.CellState, pos: Int) {
@@ -62,95 +63,39 @@ class TicTacToeFragment : Fragment() {
                 if (isBotRound) TicTacToeAdapter.CellState.O else TicTacToeAdapter.CellState.X
 
             markCell(pos, marker)
+            roundCount += 1
 
             updateScreen(marker)
 
             isBotRound = !isBotRound
 
-            if(roundCount > 5) checkResult()
-        }
-    }
+            if (roundCount > 5) {
+                when (checkStatus()) {
+                    TicTacToeManager.GameStatus.X_WON -> finishGame(TicTacToeManager.GameStatus.X_WON)
 
-    private fun checkResult() {
-        when (checkStatus()) {
-            "X" -> {
-                ticTacToeAdapter.isBoardEnabled(false)
-                Snackbar.make(view as View, getString(R.string.x_wins), Snackbar.LENGTH_LONG)
-                    .setAction("Retry") { ticTacToeAdapter.reset() }.show()
-                resetViews()
+                    TicTacToeManager.GameStatus.O_WON -> finishGame(TicTacToeManager.GameStatus.O_WON)
 
-            }
+                    TicTacToeManager.GameStatus.DRAW -> finishGame(TicTacToeManager.GameStatus.DRAW)
 
-            "O" -> {
-                ticTacToeAdapter.isBoardEnabled(false)
-                resetViews()
-                Toast.makeText(context, getString(R.string.o_wins), Toast.LENGTH_LONG).show()
-            }
-
-            "Draw" -> {
-                ticTacToeAdapter.isBoardEnabled(false)
-                resetViews()
-                Toast.makeText(context, getString(R.string.draw), Toast.LENGTH_LONG).show()
-            }
-        }
-    }
-
-    private fun checkStatus(): String {
-        val matrix = matrix
-        var result = "none"
-
-        // TODO CHANGE TO BOARD
-        // Check row
-        outer@ for (i in matrix) {
-            inner@ for (j in i.indices) {
-                if (i[j].isBlank()) {
-                    continue@outer
-                } else {
-                    if (i.size <= j + 2) {
-                        continue@outer
-                    } else {
-                        if (i[j] == i[j + 1] && i[j] == i[j + 2]) {
-                            result = i[j]
-                            return result
-                        } else {
-                            continue@inner
-                        }
-                    }
+                    TicTacToeManager.GameStatus.IN_PROGRESS -> ticTacToeAdapter.isBoardEnabled(true)
                 }
             }
         }
-
-        // Check column
-        val pos = 0
-        column@ for (j in 0..2) {
-            if (matrix[pos][j].isBlank()) {
-                continue@column
-            } else {
-                if (matrix[pos][j] == matrix[pos + 1][j] && matrix[pos][j] == matrix[pos + 2][j]) {
-                    result = matrix[pos][j]
-                    return result
-                } else {
-                    continue@column
-                }
-            }
-        }
-
-        // Check Transversal
-        if (matrix[pos][pos].isNotBlank() && matrix[pos + 2][pos + 2].isNotBlank()) {
-            if (matrix[pos][pos] == matrix[pos + 1][pos + 1] && matrix[pos][pos] == matrix[pos + 2][pos + 2]) {
-                result = matrix[pos][pos]
-                return result
-            }
-
-            if (matrix[pos][pos + 2] == matrix[pos + 1][pos + 1] && matrix[pos][pos + 2] == matrix[pos + 2][pos]) {
-                result = matrix[pos][pos]
-                return result
-            }
-        }
-
-        return if (roundCount == 10) "Draw" else result
     }
 
+    private fun finishGame(winner: TicTacToeManager.GameStatus) {
+        val result = when(winner) {
+            TicTacToeManager.GameStatus.X_WON -> getString(R.string.x_wins)
+            TicTacToeManager.GameStatus.O_WON -> getString(R.string.o_wins)
+            TicTacToeManager.GameStatus.DRAW -> getString(R.string.draw)
+            else -> throw Exception("The function needs one of the above values.")
+        }
+
+        ticTacToeAdapter.isBoardEnabled(false)
+        Snackbar.make(view as View, result, Snackbar.LENGTH_LONG)
+            .setAction("Retry") { ticTacToeAdapter.reset() }.show()
+        resetViews()
+    }
 
     private fun updateScreen(marker: TicTacToeAdapter.CellState) {
         with(binding) {
